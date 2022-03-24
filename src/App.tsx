6c5af87f8f94e3f50d0acc66535c87c543b01d34
Yurii Bodnar <yuriibodnar@MacBook-Pro-Yurii.local>
2022-03-24 17:44:32 +0200

    upDate

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+ 
+const App:React.FC<{domElement: any}> = ( { domElement }) => {
+  
+  const textColor = domElement.getAttribute("textColor");
+  const backgroundColor = domElement.getAttribute("backgroundColor");
+  const mainColor = domElement.getAttribute("mainColor");
+  const borderColor = domElement.getAttribute("borderColor");
 
-function App() {
+  const colors= {
+    textColor: textColor,
+    backgroundColor: backgroundColor,
+    mainColor: mainColor,
+    borderColor: borderColor
+  }
+  
+  console.log(colors)
+
+
+
+  const handleStart = () => {
+    console.log('Start!!!')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='hbz-widget-box'>
+      <div className='questionnare-box'>
+        <div className='content-wrapper'>
+          <div className='process'>
+            <div className='process-load'>
+            </div>
+          </div>
+          <span>
+            We are going to ask you a few question
+            s to more accurately select courses for you
+          </span>
+        </div>
+        <button className='start-button' onClick={handleStart}>START</button>
+      </div>
     </div>
   );
 }
